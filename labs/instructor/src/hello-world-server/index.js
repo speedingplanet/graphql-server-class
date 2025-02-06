ee0a3ab7107d14caec2fa3accf25d0f6bd68615d
John Paxton <johnpaxton@gmail.com>
2025-02-06 14:52:39 -0500

    parameterized schema and resolvers

---
+++ labs/instructor/src/hello-world-server/index.js
@@ -0,0 +1,17 @@
+// @ts-check
+import { ApolloServer } from '@apollo/server';
+import { startStandaloneServer } from '@apollo/server/standalone';
+import { resolvers } from './hello-world-resolvers';
+import { loadSchema } from '../load-schema';
+
+const typeDefs = loadSchema('src/hello-world-server/hello-world.graphql');
+
+const server = new ApolloServer({
+	typeDefs, // Practically, your schema
+	resolvers, // JS to implement schema responses
+});
+
+const { url } = await startStandaloneServer(server, { listen: { port: 4000 } });
+
+// console.log('Hello world server running at ' + url);
+console.log(`Hello world server running at ${url}`);

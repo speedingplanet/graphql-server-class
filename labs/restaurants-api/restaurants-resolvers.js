27aca77e5338ead9940021cabd098cced07e6222
John Paxton <johnpaxton@gmail.com>
2025-02-07 14:22:01 -0500

    Custom resolvers

--- labs/restaurants-api/restaurants-resolvers.js
+++ labs/restaurants-api/restaurants-resolvers.js
@@ -13,26 +13,63 @@ export const resolvers = {
 			return saveData(data);
 		},
 		// implement code to return an array of Restaurant objects
+		restaurants: getAllRestaurants,
 	},
 };
+
+function getAllRestaurants() {
+	return data.restaurants;
+}
 
 let data = {
 	restaurants: [
 		{
 			id: '1',
 			name: 'Stamna Taverna',
-			description: 'Good Greek food'
+			description: 'Good Greek food',
+			addressId: 4,
 		},
 		{
 			id: '2',
-			name: 'Kim\'s Sushi'
+			name: "Kim's Sushi",
+			addressId: 2,
 		},
 		{
 			id: '3',
-			name: 'Ruthie\'s BBQ'
-		}
+			name: "Ruthie's BBQ",
+			addressId: 20
+		},
 	],
-	addresses: [],
+	addresses: [
+		{
+			id: 2,
+			street: '500 Eagle Rock Ave',
+			city: 'West Orange',
+			state: 'NJ',
+			zip: '07293',
+		},
+		{
+			id: 4,
+			street: '123 Broad St',
+			city: 'Bloomfield',
+			state: 'NJ',
+			zip: '07432',
+		},
+		{
+			id: 20,
+			street: '44 State St',
+			city: 'Armonk',
+			state: 'NY',
+			zip: '10032',
+		},
+	],
+	// Like a join table
+	// restaurantsAddresses: [
+	// 	{restaurantId: 2, addressId: 2},
+	// 	{restaurantId: 10, addressId: 4},
+	// 	{restaurantId: 10, addressId: 8},
+	// 	{restaurantId: 2, addressId: 2},
+	// ],
 	menuItems: [],
 };
 
